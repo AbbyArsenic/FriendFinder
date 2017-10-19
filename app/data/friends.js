9bd9140f8b71f5f4ca607c3323a21fa7e20bcd02
Abby Johnson <brit13@gmail.com>
2017-10-19 01:28:33 -0700

    semi-final working copy

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,11 +3,49 @@
 // dummy data for testing
 var friendArray = [
   {
-    "name":"Ahmed",
-    "photo":"https://http.cat/404.jpg",
-    "scores":[
-        5,
-        1
+    "name": "Abby",
+    "photo": "http://www.arizonaderbydames.com/media/uploads/2015/11/e4c55187a13395ad917fb8026898a433.jpg",
+    "scores": [
+      "5",
+      "3",
+      "4",
+      "3",
+      "5",
+      "3",
+      "5",
+      "5",
+      "4",
+      "4"
+    ]
+  }, {
+    "name": "Jack",
+    "photo": "https://gallerymeltdown.files.wordpress.com/2010/03/jack01makesmallercopy-11.jpg",
+    "scores": [
+      "4",
+      "5",
+      "2",
+      "3",
+      "4",
+      "5",
+      "2",
+      "3",
+      "2",
+      "3"
+    ]
+  }, {
+    "name": "Ramona",
+    "photo": "http://www.theshiznit.co.uk/images/screens/scott-pilgrim-vs-the-world-299ff-large.jpg",
+    "scores": [
+      "3",
+      "3",
+      "5",
+      "2",
+      "3",
+      "5",
+      "3",
+      "4",
+      "2",
+      "3"
       ]
   }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,5 +16,26 @@ module.exports = function(app) {
   // To handle survey results and compatibility logic.
   app.post("/api/friends", function(req, res) {
     friendData.push(req.body);
+    
+    var match = "No match found";
+    var n = friendData.length-1;
+    var newFriend = friendData[n];
+
+    for(var j = 0; j < n; j++) {
+    var matchDiff = 100;
+      var difference = 0;
+      for(var i = 0; i < friendData[j].scores.length; i++) {
+        difference += Math.abs(parseInt(newFriend.scores[i]) - parseInt(friendData[j].scores[i]));
+      }
+      if (matchDiff > difference) {
+        matchDiff = difference;
+        match = friendData[j];
+      }
+      console.log(friendData[j].name + ": " + difference);
+    }
+
+    console.log(match.name);
+    res.send(match);
   });
 };
+
